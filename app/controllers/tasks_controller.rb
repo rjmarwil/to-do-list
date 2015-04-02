a21199ac07d9d34c3ee6360b8fdef24a2346a22f
Jake Marwil <rjm02006@gmail.com>
2015-04-02 11:52:32 -0600

    add index and create methods with strong params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,30 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      render json: @task
+    else
+      render json: @task, status: 500
+    end
   end
 
   def update
   end
 
   def delete
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description, :placement, :completed)
   end
 end
