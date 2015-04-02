0b92b61d7e37595b860cf6f69f7f34cb794ec84d
Jake Marwil <rjm02006@gmail.com>
2015-04-02 15:05:18 -0600

    put in return if line

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,10 +2,12 @@ class Task < ActiveRecord::Base
   validates :description, presence: true
 
   def self.swap(task, direction)
+    return if task.placement == 0 || task.placement == Task.count - 1
+
     current_place = task.placement
     other_task = Task.find_by(placement: current_place + direction)
     other_place = other_task.placement
-    
+
     other_task.update(placement: current_place)
     task.update(placement: other_place)
   end
