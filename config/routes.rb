77c7e9b3b4b7ea5123bebe4ef879076113d89a99
Jake Marwil <rjm02006@gmail.com>
2015-04-02 10:38:12 -0600

    generate controller with views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/create'
+
+  get 'tasks/update'
+
+  get 'tasks/delete'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
