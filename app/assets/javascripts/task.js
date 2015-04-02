193d621360ed694bb11ac73201d6aafc9d783275
Jake Marwil <rjm02006@gmail.com>
2015-04-02 15:06:26 -0600

    include jquery and ajax to append new tasks and to swap items in task list

--- app/assets/javascripts/task.js
+++ app/assets/javascripts/task.js
@@ -4,11 +4,26 @@ $(function() {
     $.ajax('/tasks', {
       method: 'POST',
       data: {"task": {"description": $('#task_description').val() }}
+
     }).done(function(data) {
-      $("<li>" + data.description + "</li>").appendTo('ol');
+      $("<li>" + data.description +
+      "<a rel='nofollow data-method='patch' href='/tasks/" + data.id + "swap?direction=-1'>up</a></li>" +
+      "<a rel='nofollow data-method='patch' href='/tasks/" + data.id + "swap?direction=1'>down</a></li>"
+      ).appendTo('ol');
       $('#task_description').val('');
     }).fail(function(data) {
       console.log("we failed");
     });
+  });
+
+  $("a").on('click', function(e) {
+    e.preventDefault();
+    console.log(e.target.prop('href'));
+
+    $.ajax(e.target.prop('href'), {
+      method: 'PATCH'
+    }).done(function(data) {
+      console.log(data);
+    })
   });
 });
