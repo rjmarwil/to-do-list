1c6df765c3cbad477c696b86da82201dc07d83d4
Jake Marwil <rjm02006@gmail.com>
2015-04-02 14:45:00 -0600

    create swap route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root "tasks#index"
 
-  resources :tasks
+  resources :tasks do
+    member do
+      put 'swap'
+    end
+  end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
